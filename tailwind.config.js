5ef2afa9f1afb91a5e7dd199c99698439e3a9365
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-08 20:28:22 +0200

    Initial commit from gatsby: (https://github.com/taylorbryant/gatsby-starter-tailwind.git)

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// See https://tailwindcss.com/docs/configuration for details
+module.exports = {
+  purge: [
+    './src/**/*.js',
+  ],
+  theme: {},
+  variants: {},
+  // https://github.com/tailwindcss/custom-forms
+  plugins: [require("@tailwindcss/custom-forms")],
+};
