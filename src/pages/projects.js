31d361dfec411cd629313e86059da0310515fb1a
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-10 17:33:56 +0200

    Finished the projects preview section on main page, and added a projects page for all of the projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
-import Typist from 'react-typist'
 import Grid from '@material-ui/core/Grid'
-import Image from 'gatsby-image'
-import IconButton from '@material-ui/core/IconButton';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import scrollTo from 'gatsby-plugin-smoothscroll';
+import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 
-// The top banner with profile pic, name, and some animated text
-function ProjectsPreview() {
+import Layout from "../components/layout";
+
+function Projects() {
     const [repos, setRepos] = useState([])
 
     useEffect(() => {
@@ -21,27 +18,32 @@ function ProjectsPreview() {
     }, []);
     console.log(repos)
 
-return (
-    <section id="proj-preview">
-      <Grid direction="row" justify="center" alignItems="center" container spacing={3}>
-        <Grid item lg={12} xs={12} xl={12}>
-          <b><h3 className="text-gray-400 text-center wow fadeIn">Projects</h3></b>
-        </Grid>
-        <Grid item lg={12} xl={12}>
-          <p className="text-gray-400 text-center wow fadeIn">
-            Below you can only see three of my latest projects. To see more, click the link down below ;)
-            <a href="/projects"><h6 className="text-center wow fadeIn">{">"} Press here for more! {"<"}</h6></a>
-          </p>
-        </Grid>
-        {repos.slice(0, 3).map((repo, index) => (
+  return (
+    <Layout>
+      <div>
+        <br/>
+        <Grid direction="row" justify="center" alignItems="center" container spacing={3}>
+          <Grid Grid item lg={12} xs={12} xl={12}>
+            <b><h2 className="text-gray-400 text-center wow fadeIn">Projects</h2></b>
+            <a href="/"><h6 className="text-center wow fadeIn">Return home</h6></a>
+          </Grid>
+          <Grid item lg={12} xl={12}>
+            <p className="text-gray-400 text-center wow fadeIn">
+              Below you can see all my public repositories, fetched through calls to the GitHub API. 
+            </p>
+            <p className="text-gray-400 text-center wow fadeIn">
+              To visit the repository page for any of them, click the project name. 
+            </p>
+          </Grid>
+          {repos.map((repo, index) => (
           <Grid item
             className="wow fadeIn"
             key={repo.id}
             style={{
-              animationDelay: `${index * 200 + 200}ms`,
+              animationDelay: `${index * 100 + 100}ms`,
             }}
           >
-            <Paper elevation={3} style={{width: '300px', maxHeight: '110vh', padding: '15px', background: '#212121',}}>
+            <Paper elevation={3} style={{width: '350px', maxHeight: '110vh', padding: '15px', background: '#212121',}}>
               <Grid direction="column" container spacing={2}>
                 <Grid item><i><h5><a className="text-gray-400" href={repo.html_url}>{repo.name}</a></h5></i></Grid>
                 <Grid item><h6 className="text-gray-400">{repo.description}</h6></Grid>
@@ -63,9 +65,10 @@ return (
             </Paper>
           </Grid>
         ))}
-      </Grid>
-    </section>
+        </Grid>
+      </div>
+    </Layout>
   );
 }
 
-export default ProjectsPreview;   
+export default Projects;
