b258f4219ea720702d49eeb098790b5e7feda35d
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-13 02:05:43 +0200

    Added pop-up dialogs with pictures and links to full version on gallery page!

--- src/pages/photogallery.js
+++ src/pages/photogallery.js
@@ -5,15 +5,35 @@ import Grid from '@material-ui/core/Grid'
 import SEO from '../components/seo'
 import Button from '@material-ui/core/Button'
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
-
+import Fade from '@material-ui/core/Fade';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { makeStyles } from '@material-ui/core/styles';
 import devApi from '../apiRoutes/devApi'
 import prodApi from '../apiRoutes/prodApi'
 import Layout from "../components/layout";
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(5),
+  },
+  paper: { // set colour of the paper dialog
+    backgroundColor: '#212121',
+    color: 'white'
+  }
+}));
+
 
 // The Gallery page
 function PhotoGallery() {
-  const [images, setImgs] = useState([])
+  const [images, setImgs] = useState([{lowRes: '', highRes: ''}])
+  const [selectedURLS, setUrl] = useState([])
   const BACKEND_URL = process.env.NODE_ENV === 'production' ? prodApi : devApi
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
 
   // Fetch all image URLs from Azure via my backend and append to array
   useEffect(() => {
@@ -24,6 +44,14 @@ function PhotoGallery() {
     fetchData();
   }, []);
   console.log(images)
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   
 
   return (
@@ -46,12 +74,18 @@ function PhotoGallery() {
             images.map((image, index) => (
               <Grid item
                 className="wow fadeIn"
-                key={image.URL}
+                key={image.lowRes}
                 style={{
                   animationDelay: `${index * 100 + 100}ms`,
                 }}
               >
-                <img src={image.lowRes} height="200" width="200"/>
+              <a className="cursor-pointer">
+                <img 
+                  src={image.lowRes} 
+                  height="200" width="200" 
+                  onClick={() => { setUrl({lowRes: image.lowRes, highRes: image.highRes}); handleOpen();}}
+                />
+              </a>
               </Grid>
             ))
           }
@@ -66,6 +100,21 @@ function PhotoGallery() {
             </Link>
           </Grid>
         </Grid>
+
+        {/* Dialog with the selected picture for preview and button to full size version */}
+        <Dialog classes={classes} onClose={handleClose} aria-labelledby="customized-dialog-title" maxWidth="xs" open={open}>
+          <div className={classes.paper}>
+            <img src={selectedURLS.lowRes}/>
+          </div>
+          <DialogActions>
+            <a className="text-gray-400" href={selectedURLS.highRes} rel="noopener noreferrer" target="_blank">
+              <Button size="small" color="inherit">
+                Download full version
+              </Button>
+            </a>
+            </DialogActions>
+        </Dialog>
+
         <br/><br/><br/>
       </div>
     </Layout>
