d1d7a9dd4c159c8f38b9527a1892cb020644d1d0
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-13 18:27:08 +0200

    Added picture preview dialog on main page

--- src/components/PhotosPreview.js
+++ src/components/PhotosPreview.js
@@ -19,20 +19,44 @@ import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: -35,
+    padding: theme.spacing(5),
+    backgroundColor: '#212121'
+  },
+  paper: { // set colour of the paper dialog
+    backgroundColor: '#212121',
+    color: 'white'
+  }
+}));
+
 // Show four pictures for gallery preview on main page
 const PhotosPreview = () => {
-    const [images, setImgs] = useState([{lowRes: '', highRes: ''}])
-  
-    const BACKEND_URL = process.env.NODE_ENV === 'production' ? prodApi : devApi
-    // Fetch all image URLs from Azure via my backend and append to array
-    useEffect(() => {
-        const fetchData = async () => {
-          const response = await axios.get(`${BACKEND_URL}/blobs`);
-          setImgs(response.data);
-      }
-      fetchData();
-    }, []);
-    console.log(images)
+  const [images, setImgs] = useState([{lowRes: '', highRes: ''}])
+  const [selectedURLS, setUrl] = useState([])
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+
+  const BACKEND_URL = process.env.NODE_ENV === 'production' ? prodApi : devApi
+  // Fetch all image URLs from Azure via my backend and append to array
+  useEffect(() => {
+      const fetchData = async () => {
+        const response = await axios.get(`${BACKEND_URL}/blobs`);
+        setImgs(response.data);
+    }
+    fetchData();
+  }, []);
+  console.log(images)
+
+  // Open picture preview dialog
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  // Close picture preview dialog
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <section id="photos-prev">
@@ -57,17 +81,34 @@ const PhotosPreview = () => {
             <Paper elevation={3} style={{width: '200px', maxHeight: '110vh', padding: '15px', background: '#212121',}}>
               <Grid direction="column" container spacing={2}>
                 <Grid item>
-                  <img 
-                    src={image.lowRes} 
-                    height="200" width="200" 
-                    
-                  />
+                  <a className="cursor-pointer">
+                    <img 
+                      src={image.lowRes} 
+                      height="200" width="200" 
+                      onClick={() => { setUrl({lowRes: image.lowRes, highRes: image.highRes}); handleOpen();}}
+                    />
+                  </a>
                 </Grid>
               </Grid>
             </Paper>
           </Grid>
         ))}
       </Grid>
+
+      {/* Dialog with the selected picture for preview */}
+      <Dialog 
+        classes={classes} 
+        onClose={handleClose} 
+        aria-labelledby="customized-dialog-title" 
+        open={open}
+        scroll="body"
+        maxWidth="xs"
+      >
+        <div className={classes.paper}>
+          <img className="photoPreview" src={selectedURLS.lowRes}/>
+        </div>
+      </Dialog>
+
       <br/>
       <br/>
     </section>
