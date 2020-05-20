f813dd296a1b2fe5be381f25b9a468ffc768f6c0
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-20 23:28:54 +0200

    Improved displaying of the photos preview component on smaller devices

--- src/components/PhotosPreview.js
+++ src/components/PhotosPreview.js
@@ -11,11 +11,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     margin: -35,
     padding: theme.spacing(5),
-    backgroundColor: '#212121'
+    backgroundColor: '#212121',
   },
   paper: { // set colour of the paper dialog
     backgroundColor: '#212121',
-    color: 'white'
+    color: 'white',
   }
 }));
 
@@ -64,13 +64,13 @@ function PhotosPreview() {
               animationDelay: `${index * 200 + 200}ms`,
             }}
           >
-            <Paper elevation={3} style={{width: '200px', maxHeight: '110vh', padding: '15px', background: '#212121'}}>
+            <Paper className="pic-preview-main" elevation={3} style={{padding: '8px', background: '#212121'}}>
               <Grid direction="column" container spacing={2}>
                 <Grid item>
                   <a className="cursor-pointer">
                     <img 
                       src={image.lowRes} 
-                      height="200" width="200" 
+                      height="200" width="200"
                       onClick={() => { setUrl({lowRes: image.lowRes, highRes: image.highRes}); handleOpen();}}
                     />
                   </a>
