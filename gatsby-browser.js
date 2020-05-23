92e4097c9c10bfc798a042dc2a52c831cf03895e
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-24 01:05:58 +0200

    Added new blog post

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import "./src/css/style.css";
 import "./src/css/animate.css";
 import "./src/css/languages.css";
+require("prismjs/themes/prism-okaidia.css")
 
 // trigger an immediate page refresh when an update is found
 // export const onServiceWorkerUpdateReady = () => window.location.reload();
