d6f24eafe9667fb53f139c8070d97e118e1edcbf
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-24 02:47:13 +0200

    Fixed some horrible typos

--- src/components/About.js
+++ src/components/About.js
@@ -56,11 +56,11 @@ function About() {
         <Grid item lg={9} xl={10}>
           <h5 className="text-gray-400 text-center wow fadeIn">
             Great question! My name is Piotr, although people mostly call me Peter, and I am a {age} year old programmer
-            and second year BSc Computer Science student at City, University of London,
+            and second-year BSc Computer Science student at City, University of London.
             I was originally born in Poland, but for the majority
-            of my life I&#39;ve been living in Norway. In september 2018 I moved to London to study.
-            Before I came to the UK, I graduated high school with a diploma in general studies,
-            as well as electronical and electric engineering.
+            of my life I&#39;ve been living in Norway. In September 2018 I moved to London to study.
+            Before I came to the UK I graduated high school with a diploma in general studies,
+            as well as electronics and electrical engineering.
           </h5>
           <br />
           <h5 className="text-gray-400 text-center wow fadeIn">
@@ -68,15 +68,15 @@ function About() {
             I&#39;ve been taking pictures since I can remember, and the website you&#39;re currently on, doubles as my
             photographic portfolio.
             <br />
-            In the <Link to="/photogallery">gallery</Link> section, you can view som of my shots and download them!
+            In the <Link to="/photogallery">gallery</Link> section, you can view some of my shots and download them!
           </h5>
           <br />
           <h5 className="text-gray-400 text-center wow fadeIn">
-            My passion for programming started a couple years ago, after
+            My passion for programming started a couple of years ago, after
             accidentally finding a Python instruction course on YouTube. Ever
             since I have been trying to learn new languages, concepts, and
             technologies. What amazes me most about computer science, is how
-            vast it really is, and how much you can do with even a slight
+            vast it is, and just how much you can do with even a slight
             knowledge of it. I am learning more and more every single day, both in University
             and in my spare time, through various side-projects.
           </h5>
