c4d5b3c84b777459a1d9ca6b276794c3f1e9a148
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-09-28 22:36:44 +0100

    Moved completely away from Tailwid on new projects section :building_construction:

--- src/components/OtherProjectsPreview.js
+++ src/components/OtherProjectsPreview.js
@@ -14,11 +14,14 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles({
   root: {
     width: 360,
-    background: '#212121'
+    background: '#212121',
   },
   media: {
     height: 140,
   },
+  text: {
+    color: 'white'
+  }
 });
 
 // Projects I am involved in with at work
@@ -27,35 +30,37 @@ function OtherProjectsPreview() {
 
   return (
     <section id="proj-preview">
-      <Grid direction="row" justify="center" alignItems="center" container spacing={4}>
+      <Grid direction="column" justify="center" alignItems="center" container spacing={3}>
         <Grid item lg={12} xs={12} xl={12}>
-          <b><h3 className="text-gray-400 text-center wow fadeIn">Work projects</h3></b>
+          <b><Typography variant="h4" className={classes.text}>Work projects</Typography></b>
         </Grid>
         <Grid item md={12} lg={12} xl={12} xs={12}>
         </Grid>
-        {
-          projects.map((item, i) => (
-            <Grid item md={5} lg={5}
-              className="wow fadeIn"
-              key={item.title}
-              style={{
-                animationDelay: `${i * 70 + 70}ms`,
-              }}
-            >
-              <Card className={classes.root}>
-                <CardMedia title={item.title} className={classes.media} image={item.picture} />
-                <CardContent>
-                <Typography gutterBottom className="text-gray-400" variant="h6" component="h2">
-                  {item.title}
-                </Typography>
-                <Typography variant="body2" className="text-gray-400" component="p">
-                  {item.description}
-                </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))
-        }
+        <Grid container item direction="row" justify="center" alignItems="center" spacing={4}>
+          {
+            projects.map((item, i) => (
+              <Grid item md={6} lg={6}
+                className="wow fadeIn"
+                key={item.title}
+                style={{
+                  animationDelay: `${i * 70 + 70}ms`,
+                }}
+              >
+                <Card className={classes.root}>
+                  <CardMedia title={item.title} className={classes.media} image={item.picture} />
+                  <CardContent>
+                  <Typography gutterBottom className={classes.text} variant="h6" component="h2">
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body2" className={classes.text} component="p">
+                    {item.description}
+                  </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
       </Grid>
       <br />
       <br />
