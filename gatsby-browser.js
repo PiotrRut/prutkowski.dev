7ca80a70e922457923d7981db53ea3099ecf3186
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-10-04 14:01:47 +0100

    Screw gatsby service workers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,6 @@ import './src/css/animate.css';
 import './src/css/languages.css';
 import './src/css/prism-okaidia.css';
 
-// trigger an immediate page refresh when an update is found
+// This will invalidate old site cache and trigger a refresh everytime there is a new update pushed,
+// meaning visitors to the site will always be presented with the most up to date content.
 export const onServiceWorkerUpdateReady = () => window.location.reload();
