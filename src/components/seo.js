a47e54ec5088649b41c4419ee782d8d663e1bd0f
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-10-05 19:47:08 +0100

    Social updates :sparkles:

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,7 +31,7 @@ const SEO = ({ title, description, image, article }) => {
 
       {seo.url && <meta property="og:url" content={seo.url} />}
 
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+      <meta property="og:type" content="article" />
 
       {seo.title && (
         <meta property="og:title" content={`${defaultTitle} | ${title}`} />
