13972127e81f59ec75e9e766bed4129015440cb7
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-12-17 21:10:34 +0100

    Tiny change :pencil2:

---
+++ src/misc/projects.js
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    picture: 'FXHome.webp',
+    title: 'FairFX Multicurrency Web',
+    description:
+      'The FairFX Multicurrency web app makes it easy to manage, pause and replace your multicurrency card online, along with addding and exchanging money on the go!',
+    path: 'https://app.fairfx.com',
+    team: 'Jamie Halvorson, Mahedi Hasan, Tabasom Aryamanesh',
+  },
+  {
+    picture: 'Components.webp',
+    title: 'Geometry Web',
+    description:
+      'The Geometry component library provides reusable design components for the dev teams to use across their projects, giving all a uniform experience and look.',
+    team: 'Jamie Halvorson, Mahedi Hasan, Tabasom Aryamanesh',
+  },
+];
