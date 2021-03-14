2ce5b6b7d79b7ffbb1338acbacd5a4bf97251f5a
PiotrRut <piotr.rutkowski@city.ac.uk>
2021-03-14 14:27:53 +0000

    Fix typo to fix webkit styles :unamused:

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -6,7 +6,7 @@ const Header = styled.header`
   color: transparent;
   background-image: linear-gradient(to right, #005e7f, #61b6cd);
   background-clip: text;
-  --webkit-background-clip: text;
+  -webkit-background-clip: text;
   padding-bottom: 2rem;
 `;
 
