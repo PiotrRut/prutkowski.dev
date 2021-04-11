52c58cb6c19cd2ab3ac392260903749da20457c3
PiotrRut <piotr.rutkowski@city.ac.uk>
2021-04-11 22:21:47 +0100

    feat: Update for smaller breakpoints

--- src/components/ImageCard/ImageCard.styles.ts
+++ src/components/ImageCard/ImageCard.styles.ts
@@ -11,14 +11,18 @@ export const ImageCardRoot = styled.div`
   margin: 0 ${sizes['15']} ${sizes['15']};
 
   @media (max-width: 990px) {
-    /* height: ${sizes['200']};
-    width: ${sizes['150']}; */
     margin: 0 ${sizes['5']} ${sizes['10']};
   }
 
   @media (max-width: 480px) {
     height: ${sizes['200']};
     width: ${sizes['150']};
+    margin: 0 ${sizes['5']} ${sizes['10']};
+  }
+
+  @media (max-width: 375px) {
+    height: ${sizes['180']};
+    width: ${sizes['130']};
     margin: 0 ${sizes['5']} ${sizes['10']};
   }
 `;
